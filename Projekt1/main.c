a03a1fa73fa0658ee8ceafa2a095ad4506db47a8
Bartek Zieleziński <bartosz.zielezinski@gmail.com>
2018-11-17 20:24:00 +0100

    Dodaj pliki projektów.

---
+++ Projekt1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int i_choice;
+		i_flname;
+	do
+	{
+		printf("1. Open an image\n2.Exit\n");
+		scanf("%d", &i_choice);
+		switch (i_choice)
+		{
+		case 1: 
+		{
+			printf("Insert a name of a desired file: ");
+			scanf()
+		}
+		}
+
+	} while (i_choice!=2)
+}
